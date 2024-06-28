cc5d46a94561c8b978037869e8470a0d1adfd766
Marko Mutavdzic <belikompijuter@gmail.com>
2024-06-28 14:27:58 +0200

    Initial Commit

---
+++ GreekKino/GreekKinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreekKinoApp.swift
+//  GreekKino
+//
+//  Created by Marko on 28.6.24..
+//
+
+import SwiftUI
+
+@main
+struct GreekKinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
